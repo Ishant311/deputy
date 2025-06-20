d7c0e51c6bbdf225c02bb71960e4b0a7348b0eb5
Ishant Arora <ishantarora311@gmail.com>
2025-06-20 10:18:13 +0530

    Bug Fixed for home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,10 @@ import HeroSection from '../components/Home/HeroSection'
 import StatsSection from '../components/Home/StatsSection'
 import DeputySection from '../components/Home/Deputy'
 import TrustedBrandsSection from '../components/Home/TrustedBrands'
-import TimesheetCompliance from '../components/Home/Timesheet'
+import TimesheetCompliance from '../components/Home/TimeSheet'
 import ReviewSection from '../components/Home/ReviewSection'
 import Ratings from '../components/Home/ratings'
 import LearnMoreSection from '../components/Home/LearnMore'
-import Footer from '../components/Footer'
 
 function Home() {
   return (
